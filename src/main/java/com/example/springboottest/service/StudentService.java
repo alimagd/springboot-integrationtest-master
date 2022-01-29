94da9592f4737bb10a5060d3208ef871dac42188
ALex MAJIDI <mailofmajidi@gmail.com>
2022-01-29 20:07:20 +0000

    Commit shows Integration Testing

---
+++ src/main/java/com/example/springboottest/service/StudentService.java
@@ -0,0 +1,36 @@
+package com.example.springboottest.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.springboottest.model.Student;
+
+@Component
+public class StudentService {
+
+    @Autowired
+    private StudentRepository repository;
+
+    public List<Student> createStudent() {
+        List<Student> students = new ArrayList<Student>();
+        List<Student> savedStudents = new ArrayList<Student>();
+        
+        students.add(new Student("Ali Joon", "Desc for st111"));
+        students.add(new Student("Fatti Magd", "Desc 4 st656"));
+        students.add(new Student("Hamid Bro", "Dsc 4 st0989"));
+        Iterable<Student> itrStudents=repository.saveAll(students);
+        itrStudents.forEach(savedStudents::add);
+
+        return savedStudents;
+    }
+
+    public Student retrieveStudent(Integer studentId) {
+       return repository.findById(studentId).orElse(new Student());
+  
+    }
+ 
+}
